75a9cf6f0ac37960b87536cc01da0871fc577679
알파 <arpa1705@gmail.com>
2024-07-24 22:24:20 +0900

    chore: styled-components 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,31 @@
+'use client'
+
 import Image from 'next/image'
 import logo from '@/aseets/logo.png'
+import styled from 'styled-components'
 
 export default function HomePage() {
   return (
     <>
-      <div className="container max-w-screen-xl h-screen mx-auto px-4 py-24">
-        <div className="flex items-center gap-12 h-full">
-          <div className="w-1/2">
-            <Image src={logo} alt="logo" />
-            <div className="py-8 text-2xl font-light">
-              모두의 건강한 식사를 위한 영양 분석 및 식단 추천 서비스
-            </div>
-
-            <div>로그인하여 건강한 식습관 관리를 시작하세요!</div>
-          </div>
-          <div className="w-1/2 p-12 h-full bg-white/50 rounded-xl shadow-xl">
-            <div className="flex justify-center">
-              <Image src={logo} alt="logo" className="w-24" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Row className="flex items-center gap-12 h-full">
+          <Image src={logo} alt="logo" />
+        </Row>
+      </Container>
     </>
   )
 }
+
+const Container = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 0 1rem;
+  height: 100vh;
+`
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  height: 100%;
+`
