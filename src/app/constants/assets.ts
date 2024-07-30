84bc5e3d6a6242a4da8d09dafa33bfa2347bbc29
알파 <arpa1705@gmail.com>
2024-07-31 00:58:57 +0900

    chore: 에셋 추가

---
+++ src/app/constants/assets.ts
@@ -0,0 +1,7 @@
+export { default as logo } from '@/assets/logo.png'
+export { default as logoWhite } from '@/assets/logo-white.png'
+export { default as logoPrimary } from '@/assets/logo-primary.png'
+export { default as googleLogo } from '@/assets/brands/google.png'
+export { default as kakaoLogo } from '@/assets/brands/kakao.png'
+export { default as naverLogoWhite } from '@/assets/brands/naver-white.png'
+export { default as appleLogoWhite } from '@/assets/brands/apple-white.png'
