e20e340a972db87f12312a69f3de693f0f144113
Buyeon Hwang <arpa1705@gmail.com>
2024-08-02 04:07:37 +0900

    fix: use clsx instead of classNames

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,29 @@
-import classNames from 'classnames'
+import clsx from 'clsx'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary' | 'secondary'
+  variant?: 'primary' | 'secondary' | 'outlined'
 }
 
 export default function Button({ children, ...props }: ButtonProps) {
+  let variantStyle
+
+  switch (props.variant) {
+    case 'primary':
+      variantStyle = 'bg-primary-600 text-white'
+      break
+    case 'secondary':
+      variantStyle = 'bg-white text-black border border-black'
+      break
+    case 'outlined':
+      variantStyle = 'bg-transparent text-primary-600 border border-primary-600'
+      break
+    default:
+      variantStyle = 'bg-primary-600 text-white'
+      break
+  }
+
   return (
-    <button
-      {...props}
-      className={classNames(
-        'px-6 py-3 rounded-xl',
-        props.variant === 'primary'
-          ? 'bg-primary-600 text-white'
-          : 'bg-white text-black border border-black'
-      )}
-    >
+    <button {...props} className={clsx('px-6 py-2 rounded-xl', variantStyle)}>
       {children}
     </button>
   )
