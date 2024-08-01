e65cdd42adf677a51b073c427aa64d7d892e78b1
Buyeon Hwang <arpa1705@gmail.com>
2024-08-01 20:18:54 +0900

    feat: 게시글 카드 컴포넌트

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 }
 
 export default nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
+import ArticleCard from '@/components/ArticleCard'
 import Layout from '@/components/Layout'
 
 export default function BoardPage() {
   return (
     <Layout>
-      <div className="container text-3xl mx-auto py-6 h-[60vh]">
-        게시판 페이지
+      <div className="container mx-auto py-6 h-[60vh]">
+        <div className="grid grid-cols-4 gap-4">
+          <ArticleCard
+            title="게시판"
+            description="게시판입니다."
+            date="2021-10-10"
+          />
+          <ArticleCard
+            title="게시판"
+            description="게시판입니다."
+            date="2021-10-10"
+          />
+          <ArticleCard
+            title="게시판"
+            description="게시판입니다."
+            date="2021-10-10"
+          />
+          <ArticleCard
+            title="게시판"
+            description="게시판입니다."
+            date="2021-10-10"
+          />
+        </div>
       </div>
     </Layout>
   )
